aaf8cc49b3ea25bbe6fd8a5386c5ab0be2991676
Muaviyah Arsalan <133225138+binSallushan@users.noreply.github.com>
2025-11-24 23:05:39 +0500

    Add EncryptText and DecryptText functions
    Implemented encryption and decryption logic

--- main.c
+++ main.c
@@ -18,3 +18,38 @@ int InputMenuChoice();
 char *InputText();
 int InputYesNo();
 char *InputFileName();
+
+
+// Encrypt text: shift letters 1 backward
+void EncryptText(char *text)
+{
+    if (!text)
+        return;
+
+    for (int i = 0; text[i] != '\0'; i++)
+    {
+        char c = text[i];
+
+        if (isupper(c))
+            text[i] = ((c - 'A' - 1 + 26) % 26) + 'A';
+        else if (islower(c))
+            text[i] = ((c - 'a' - 1 + 26) % 26) + 'a';
+    }
+}
+
+// Decrypt text: shift letters 1 forward
+void DecryptText(char *text)
+{
+    if (!text)
+        return;
+
+    for (int i = 0; text[i] != '\0'; i++)
+    {
+        char c = text[i];
+
+        if (isupper(c))
+            text[i] = ((c - 'A' + 1) % 26) + 'A';
+        else if (islower(c))
+            text[i] = ((c - 'a' + 1) % 26) + 'a';
+    }
+}
